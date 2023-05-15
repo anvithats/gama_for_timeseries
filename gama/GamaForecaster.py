a26ae653e5929e0a540a470a313eccffe505107f
anvithats <92322368+anvithats@users.noreply.github.com>
2023-05-15 10:49:59 +0200

    Update GamaForecaster.py

--- gama/GamaForecaster.py
+++ gama/GamaForecaster.py
@@ -1,8 +1,9 @@
+import inspect
 import pandas as pd
-
 from .gama import Gama
 from gama.configuration.timeseries import timeseries_config
 from gama.utilities.metrics import scoring_to_metric
+from sktime.base import BaseEstimator
 
 
 class GamaForecaster(Gama):
@@ -12,12 +13,22 @@ class GamaForecaster(Gama):
             config = timeseries_config
 
         self._metrics = scoring_to_metric(scoring)
+        if any(metric.requires_probabilities for metric in self._metrics):
+            config = {
+                alg: hp
+                for (alg, hp) in config.items()
+                if not (
+                        inspect.isclass(alg)
+                        and issubclass(alg, BaseEstimator)
+                        and not hasattr(alg(), "predict_proba")
+                )
+            }
         super().__init__(*args, **kwargs, config=config, scoring=scoring)
 
     def _predict(self, fh=None, x=None):
-        y = self.predict(fh)
+        y = self.model.predict(fh)
         return y
 
-    def fit(self, y: pd.DataFrame, fh=None, x=None, *args, **kwargs):
-        super().fit(y, fh, x, *args, **kwargs)
-        # return self
+    def fit(self, y: pd.DataFrame, x, fh=None, *args, **kwargs):
+        super().fit(x, y, fh, *args, **kwargs)
+
