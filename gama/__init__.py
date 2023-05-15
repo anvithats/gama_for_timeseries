5b69f589e4838b88f3e6910c12c8293bf3b21c04
anvithats <92322368+anvithats@users.noreply.github.com>
2023-05-15 15:15:18 +0200

    Update __init__.py

--- gama/__init__.py
+++ gama/__init__.py
@@ -1,5 +1,6 @@
 from .GamaClassifier import GamaClassifier
 from .GamaRegressor import GamaRegressor
+from .GamaForecaster import GamaForecaster
 from .__version__ import __version__
 
 name = "gama"
